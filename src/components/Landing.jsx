109783a4e807c408061669310a91fd20af68d9a5
Paul Lorber <paul.lorber@protonmail.com>
2024-03-17 01:40:04 +0100

    styles and idnex.css cleanup

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,15 +1,21 @@
 import { NavLink } from "react-router-dom";
+import YourPokemons from "./YourPokemons";
+import style from '../styles/Landing.module.css';
 
 function Landing() {
   return (
     <>
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/pokedex">Pokedex</NavLink>
-      <NavLink to="leaderboard">Leaderboard</NavLink>
+    <nav className={style.nav}>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/pokedex">Pokedex</NavLink>
+        <NavLink to="leaderboard">Leaderboard</NavLink>
+        <button>🌑</button>
+      </nav>
       <div>
-        <h1>Welcome to our site!</h1>
-        <p>We are your one-stop-shop for all things so-and-so.</p>
+        <h1>Pokefight!</h1>
+        <p></p>
       </div>
+      <YourPokemons />
     </>
   );
 }
---
+++ src/components/YourPokemons.jsx
@@ -0,0 +1,10 @@
+function YourPokemons () {
+  return (
+    <div>
+      <h2>Your Pokemons</h2>
+      <p>Here you will see all the pokemons you have captured</p>
+    </div>
+  )
+}
+
+export default YourPokemons
