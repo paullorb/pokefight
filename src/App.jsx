5d785065a97f7fd688c48981372921362750699d
Paul Lorber <paul.lorber@protonmail.com>
2024-03-17 01:28:01 +0100

    landing things

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
+import { BrowserRouter } from 'react-router-dom'
+
 import './App.css'
+import Landing from './components/Landing'
 
 function App() {
 
   return (
-    <>
+    <BrowserRouter>
       <Landing />
-    </>
+    </BrowserRouter>
   )
 }
 
